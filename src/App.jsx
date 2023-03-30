3e9982f45bd27996acbb5ba8f89b3df97a07529c
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-03-30 20:31:56 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SideCart from "./components/SideCart/SideCart";
 import Home from "./components/Home/Home";
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  // for showing watch time in UI
+  const [watchTime, setWatchTime] = useState("");
+
+  // setting watch time in local storage by button click
   const handleWatchTime = (time) => {
-    console.log(time);
+    // checking
+    const previousWatchTime = JSON.parse(localStorage.getItem("watchTime"));
+    if (previousWatchTime) {
+      const sumOfTime = previousWatchTime + time;
+      localStorage.setItem("watchTime", sumOfTime);
+      setWatchTime(sumOfTime);
+    } else {
+      localStorage.setItem("watchTime", time);
+      setWatchTime(time);
+    }
   };
   return (
     <div className="App">
@@ -14,9 +30,10 @@ function App() {
           <Home handleWatchTime={handleWatchTime}></Home>
         </div>
         <div className="side-cart card col-4 mt-2">
-          <SideCart></SideCart>
+          <SideCart watchTime={watchTime}></SideCart>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
--- src/components/SideCart/SideCart.jsx
+++ src/components/SideCart/SideCart.jsx
@@ -1,9 +1,57 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-const SideCart = () => {
+const SideCart = ({ watchTime }) => {
+  const [time, setTime] = useState([]);
+  // add break time state
+  const [breakTime, setBreakTime] = useState(0);
+
+  useEffect(() => {
+    const getWatchTimeFromStorage = localStorage.getItem("watchTime");
+    setTime(getWatchTimeFromStorage);
+
+    // get break time from local storage
+    const getBreakTime = localStorage.getItem("breakTime");
+    setBreakTime(getBreakTime);
+  }, [watchTime]);
+
+  // set break time to local storage
+  const handleBreakTime = (bt) => {
+    localStorage.setItem("break-time", bt);
+    setBreakTime(bt);
+  };
+
+  // set react-toastify in complete button
+  const handleComplete = () => {
+    toast("Wow so easy!");
+  };
+
   return (
     <div className="">
-      <div className=" text-center mt-4">This is Side Cart</div>
+      <div>
+        <h1>My Cart</h1>
+
+        <div className="mt-5 text-center">
+          <p>Total Watch Time</p>
+          <input type="text" value={time} disabled />
+        </div>
+        <h5 className="mt-5">Add Break Time</h5>
+        <button
+          onClick={() => handleBreakTime(15)}
+          className="w-25 btn-circle m-1 btn btn-info"
+        >
+          15
+        </button>
+        <button className="w-25 btn-circle bg-warning btn btn-info">20</button>
+        <button className="w-25 btn-circle m-1 bg-danger btn btn-info">
+          25
+        </button>
+        <input type="text" value={breakTime} disabled />
+        <button onClick={handleComplete} className="mt-5 btn btn-info w-100">
+          Complete
+        </button>
+      </div>
     </div>
   );
 };
